9b5f046f2db32c6303edadd576859fd35087dff5
Анастасия <nastya19.04.2000@gmail.com>
2018-12-09 01:21:53 +0300

    (-.-)

--- 05-arrays-1d/main.cpp
+++ 05-arrays-1d/main.cpp
@@ -3,27 +3,27 @@
 using namespace std;
 
 int main() {
-	int const n = 12;
-	int arr[n] = { -3, 32, 8, 0, 45, 23, -12, 1, 0, 10, 15,-6 };
+	int const kArraySize = 12;
+	int arr[kArraySize] = { -3, 32, 8, 0, 45, 23, -12, 1, 0, 10, 15,-6 };
 	cout << "Initial array: ";
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < kArraySize; i++)
 		cout << arr[i] << " ";
 	
 	int arr_min = arr[0];
 
-	for (int i = 1; i < n; i++)
+	for (int i = 1; i < kArraySize; i++)
 		if (arr[i] < arr_min) arr_min = arr[i];
 	cout << "\nmin = " << arr_min << endl;
 
 	int firstpos = -1, lastpos = -1;
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < kArraySize; i++)
 		if (arr[i] > 0)
 		{
 			firstpos = i;
 			break;
 		}
-	for (int i = n - 1; i >= 0; i--)
+	for (int i = kArraySize - 1; i >= 0; i--)
 		if (arr[i] > 0)
 		{
 			lastpos = i;
@@ -42,13 +42,13 @@ int main() {
 		cout << "\nNo two positive elements were found.";
 	}
 
-	for (int i = 0; i < n - 1; i++)
-		for (int j = 0; j < n - i - 1; j++)
+	for (int i = 0; i < kArraySize - 1; i++)
+		for (int j = 0; j < kArraySize - i - 1; j++)
 			if (arr[j] != 0 && arr[j + 1] == 0)
 				swap(arr[j], arr[j + 1]);
 
 	cout << "\nchanged array: ";
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < kArraySize; i++)
 		cout << arr[i] << " ";
 	cout << endl;
 
